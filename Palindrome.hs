a7a9957c4f486645e556b1c050ba212b4fe58d42
Noah Luck Easterly <noah.easterly@gmail.com>
2012-01-01 19:15:19 -0500

    modified for list input and lazy output

--- Palindrome.hs
+++ Palindrome.hs
@@ -1,57 +1,46 @@
 module Palindrome where
+-- an adaptation of Johan Jeuring's algorithm for finding maximal palindrome lengths
+-- (http://johanjeuring.blogspot.com/2007/08/finding-palindromes.html)
 
-import Data.Array
-import Debug.Trace
+import Control.Arrow ((***))
 
-input :: Array Int Char
-input = listArray (0,length s - 1) s
-  where s = "abacabadabacaba"
-
-showState a n m = p ++ "(" ++ b ++ ")" ++ s
-  where (p,w) = splitAt (n - m) $ elems a
-        (b,s) = splitAt m w
-
-longestPalindromes  a = extendTail afirst 0 []
-  where (afirst,alast) = bounds a
-        extendTail n currentTail centres = 
-            trace ("extendTail     : " ++ showState a n currentTail ++ " " ++ show centres) (extendTail' n currentTail centres)
-          where extendTail' n currentTail centres
-                        | n > alast                    =
-                            -- reached the end of the array
-                            finalCentres currentTail centres (currentTail:centres)
-                        | n-currentTail == afirst      =
-                            -- the current longest tail palindrome
-                            -- extends to the start of the array
-                            extendCentres n (currentTail:centres) centres currentTail
-                        | trace ("compare        : " ++ replicate (n-currentTail-1) ' ' ++ "^" ++ replicate (currentTail+2) '-' ++ "^") (a!n == a!(n-currentTail-1))   =
-                            -- the current longest tail palindrome
-                            -- can be extended
-                            extendTail (n+1) (currentTail+2) centres
-                        | otherwise                    =
-                            -- the current longest tail palindrome
-                            -- cannot be extended
-                            extendCentres n (currentTail:centres) centres currentTail
-        extendCentres n centres tcentres centreDistance = 
-            trace ("extendCenteres : " ++ showState a n centreDistance ++ " " ++ show centres ++ " " ++ show tcentres) (extendCentres' n centres tcentres centreDistance)
-          where extendCentres' n centres tcentres centreDistance
-                    | centreDistance == 0                =
-                        -- the last centre is on the last element:
-                        -- try to extend the tail of length 1
-                        extendTail (n+1) 1 centres
-                    | centreDistance-1 == head tcentres  =
-                        -- the previous element in the centre list
-                        -- reaches exactly to the end of the last
-                        -- tail palindrome use the mirror property
-                        -- of palindromes to find the longest tail
-                        -- palindrome
-                        extendTail n (head tcentres) centres
-                    | otherwise                          =
-                        -- move the centres one step
-                        -- add the length of the longest palindrome
-                        -- to the centres
-                        extendCentres n (min (head tcentres) (centreDistance-1):centres) (tail tcentres) (centreDistance-1)
-
-        finalCentres n tcentres centres = 
-            trace ("finalCenteres  : " ++ showState a (alast + 1) n ++ " " ++ show centres ++ " " ++ show tcentres) (finalCentres' n tcentres centres)
-          where finalCentres' 0     tcentres centres  =  centres
-                finalCentres' n     tcentres centres  = finalCentres (n-1) (tail tcentres) (min (head tcentres) (n-1):centres)
+-- find the maximal lengths of the palindromes
+-- centered before, on, and after each element in the list
+maximalPalindromeLengths :: Eq a => [a] -> [Int]
+maximalPalindromeLengths as = grow 0 [] as as []
+  where -- grow n lz rz as log
+        --    n     - confirmed length of palindrome at current position
+        --    lz rz - zipper for elements at the start of the palindrome
+        --    as    - elements after the palindrome
+        --    log   - reversed list of maximal palindrome lengths found so far
+        --
+        -- The log is kept to exploit the mirror property of palindromes:
+        -- any item contained in a palindrome that occurs to the left of the
+        -- center reoccurs in the mirror position to the right of the center.
+        --
+        -- This is also true for any other palindrome whose center overlaps
+        -- the first palindrome - they have a matching palindrome that mirrors
+        -- them on the other side of the first's center (though the palindrome
+        -- may be extended or truncated if it is not wholey contained in the
+        -- first).
+        grow :: Eq a => Int -> [a] -> [a] -> [a] -> [Int] -> [Int]
+        -- if the items bordering the palindrome match,
+        -- extend the palindrome to include those items and continue
+        grow n (a':lz) rz (a:as) log | (a' == a) = grow (n+2) lz (a':rz) as log
+        -- if we've reached the end of the list
+        -- replay as much as the log as possible, trimming 
+        -- lengths that would run over
+        grow n lz rz [] log = n : zipWith min log [n-1,n-2..0]
+        -- if we can't expand an empty palindrome, 
+        -- try growing the next non-empty palindrome
+        grow 0 lz rz as log = 0 : grow 1 lz rz (tail as) (0:log)
+        -- if we can't expand a non-empty palindrome,
+        -- any preceeding palindromes that are wholey contained within
+        -- this one are repeated (in reverse order) on the other side of the center
+        -- while the boarding one is the kernel for the next to grow
+        grow n lz rz as log = n : 
+            let (replay, n') = (map fst *** (uncurry min . head)) . 
+                               break (uncurry (>=)) $ 
+                               zip log [n-1,n-2..0]
+                (xs,ys) = splitAt (n - n') rz
+            in replay ++ grow n' (reverse xs ++ lz) ys as (reverse replay ++ n : log)
